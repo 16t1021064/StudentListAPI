b0e233782658e8859c06b7477baea1074e8bbb2a
Vo Huy <16t1021064@husc.edu.vn>
2022-04-25 11:38:10 +0700

    done add

--- controllers/Student.js
+++ controllers/Student.js
@@ -1,58 +1,62 @@
-const Student = require('../models/Student');
-const logger = require('../utils/logger');
+const Student = require("../models/Student");
+const logger = require("../utils/logger");
 
 const listStudent = (req, res) => {
-  Student
-    .find({})
+  Student.find({})
     .select()
-    .then(students => {
+    .then((students) => {
       res.json(students);
     })
-    .catch(err => {
+    .catch((err) => {
       logger.error(err);
       res.status(400).send(err.errors);
     });
-}
+};
 
 const getStudent = (req, res) => {
   Student.findById(req.params.id)
-		.then(student => {
-			res.json(student);
-		})
-		.catch(err => {
-			logger.error(err);
-			res.status(400).send(err.errors);
-		});
-}
+    .then((student) => {
+      res.json(student);
+    })
+    .catch((err) => {
+      logger.error(err);
+      res.status(400).send(err.errors);
+    });
+};
 
 const createStudent = (req, res) => {
-
   const newStudent = new Student(req.body);
 
-  newStudent.save(err => {  
+  newStudent.save((err) => {
     if (err) return res.status(500).send(err);
     return res.status(200).send(newStudent);
   });
-}
+};
 
-const updateStudent = (req, res) => {
-
-}
+const updateStudent = async (req, res) => {
+  try {
+    const temp = await Student.findById(req.params.id);
+    await temp.update({ $set: req.body });
+    res.status(200).send(temp);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
 
 const deleteStudent = (req, res) => {
   Student.findByIdAndRemove(req.params.id, (err, data) => {
     if (err) return res.status(500).send(err);
     const response = {
-        message: "User successfully deleted"
+      message: "Student successfully deleted",
     };
     return res.status(200).send(response);
-  })
-}
+  });
+};
 
 module.exports = {
   listStudent,
   getStudent,
   createStudent,
   deleteStudent,
-  updateStudent
-}
+  updateStudent,
+};
